9179910d827314c067bf533406e3d1bc34a5ce54
Nohan ahmed <mdpolashislam152@gmail.com>
2024-09-25 00:50:21 +0600

    Done React router and create new project for context api"

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,10 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Layout from './Layout'
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Github, { githubInfoLoader } from './components/Github/Github'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [{
+//       path: '',
+//       element: <Home />
+//     }, { 
+//       path: 'about/', 
+//       element: <About />, 
+//     }, 
+//     {
+//       path:'contact/',
+//       element:<Contact />
+//     }]
+//   }
+// ])
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='github' element={<Github />} loader={githubInfoLoader} />
+      <Route path='user/:UserId' element= {<User />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
